c014679fbb8f74ecb3e2499280ba6dd6e4d5a4bf
CT <57823605+Kanzaki-H-SCT@users.noreply.github.com>
2023-08-07 02:38:34 +0800

    Fixed init service bug.

---
+++ services/__init__.py
@@ -0,0 +1,16 @@
+from .client import LINE
+from .channel import Channel
+from .oepoll import OEPoll
+from akad.ttypes import OpType
+
+__modifier__          = 'CT'
+__copyright__         = 'Copyright 2023 by CT Core Team'
+__modifier_email__    = 'noreply.ctcoreteam@gmail.com'
+__version__           = 'iota'
+__previous_version__  = '3.0.8'
+__license__           = 'BSD-3-Clause'
+__author__            = 'Fadhiil Rachman'
+__author_email__      = 'fadhiilrachman@gmail.com'
+__url__               = 'http://github.com/fadhiilrachman/line-py'
+
+__all__ = ['LINE', 'Channel', 'OEPoll', 'OpType']
